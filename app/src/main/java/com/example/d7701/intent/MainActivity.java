224170da1102f6d422a7479e39a8bbf2b7e180c4
D7701 <meiluup@naver.com>
2018-04-11 09:23:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/d7701/intent/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.d7701.intent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Button1(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://naver.com"));
+        startActivity(intent);
+    }
+
+    public void Button2(View view) {
+        Intent intent2 = new Intent(Intent.ACTION_VIEW,Uri.parse("tel:010-3997-7195"));
+        startActivity(intent2);
+    }
+}
